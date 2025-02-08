24227d39d99e357249cf815faaac8bda499e4f3e
burkeow <burkeowen98@gmail.com>
2025-02-08 15:08:17 +1100

    create a dns question response and set fields using the query question

--- src/dns_server/server.rs
+++ src/dns_server/server.rs
@@ -32,16 +32,21 @@ impl Server {
                     response_header.answer_record_count = 1;
 
                     response_buffer[0..12].copy_from_slice(&response_header.to_bytes());
-                    let dns_question =
+                    let query_question =
                         DnsQuestion::decode_dns_question(&receive_buffer[12..]).unwrap();
-                    let dns_question_bytes = dns_question.to_bytes();
+                    let dns_question_bytes = query_question.to_bytes();
+
+                    let mut response_question = query_question;
+                    response_question.question_type = 1;
+                    response_question.class = 1;
+
                     response_buffer[12..dns_question_bytes.len() + 12]
                         .copy_from_slice(dns_question_bytes.as_slice());
 
                     let answer = DnsAnswer::new(
-                        dns_question.domain_name,
-                        dns_question.question_type,
-                        dns_question.class,
+                        response_question.domain_name,
+                        1,
+                        1,
                         60,
                         vec![8, 8, 8, 8],
                     );
