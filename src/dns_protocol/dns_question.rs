ca347089e805abc63ce2b453db7e3f7d5297204c
burkeow <burkeowen98@gmail.com>
2025-02-16 18:56:46 +1100

    add unit tests for dns question compression and fix iteration

--- src/dns_protocol/dns_question.rs
+++ src/dns_protocol/dns_question.rs
@@ -31,49 +31,61 @@ pub fn decode_questions(buf: &[u8], number_of_questions: u16) -> Option<Vec<DnsQ
     //todo lots of copying going on here
     println!("Parsing {} questions", number_of_questions);
     let mut questions = Vec::<DnsQuestion>::new();
-    let mut iter = buf.iter();
-    for _ in 0..number_of_questions {
+    let mut label_iter = buf.iter();
+    let mut end_of_label_iter = buf.iter();
+    for index in 0..number_of_questions {
         let mut labels = Vec::<Label>::new();
-        while let Some(length) = iter.next() {
+        let mut is_compressed = false;
+        while let Some(length) = label_iter.next() {
             if *length == 0x00 {
                 // iter.next();
+                if is_compressed {
+                    end_of_label_iter.next();
+                }
+                let y = end_of_label_iter.next()?;
                 break;
             }
             let (content_len, content) = if *length & 0b11000000 != 0 {
-                //compressed label
-                println!("is compressed label");
-                let offset = (((*length & 0b00111111) as u16) << 8 | *iter.next()? as u16) - 12; //or 16? 16 passed the CI but i don't think it's right
-                let mut label_iter = buf.iter().skip(offset as usize);
+                is_compressed = true;
+                let offset =
+                    (((*length & 0b00111111) as u16) << 8 | *label_iter.next()? as u16) - 12;
+                label_iter = buf.iter();
+                label_iter.nth(offset as usize - 1);
                 let label_len = *label_iter.next()?;
-                (
-                    label_len,
-                    label_iter
-                        .take(label_len as usize)
-                        .map(|&x| x as char)
-                        .collect(),
-                )
+                let label = label_iter
+                    .clone()
+                    .take(label_len as usize)
+                    .map(|&x| x as char)
+                    .collect();
+                label_iter.nth(label_len as usize - 1);
+                (label_len, label)
             } else {
-                let content = iter
+                let content = label_iter
                     .clone()
                     .take(*length as usize)
                     .map(|&x| x as char)
                     .collect();
-                iter.nth(*length as usize - 1);
+                label_iter.nth(*length as usize - 1);
                 (*length, content)
             };
+            if !is_compressed {
+                end_of_label_iter.nth(*length as usize).unwrap();
+            }
             println!("content_len: {}, content: {}", content_len, content);
             labels.push(Label {
                 length: content_len,
                 content,
             });
         }
-        let question_type = (*iter.next()? as u16) << 8 | (*iter.next()? as u16); //todo handle the endianness correctly/platform agnostically, htons?
-        let class = (*iter.next()? as u16) << 8 | (*iter.next()? as u16); //todo handle the endianness correctly/platform agnostically, htons?
+        let question_type =
+            u16::from_be_bytes([*end_of_label_iter.next()?, *end_of_label_iter.next()?]);
+        let class = u16::from_be_bytes([*end_of_label_iter.next()?, *end_of_label_iter.next()?]);
         questions.push(DnsQuestion {
             domain_name: labels,
             question_type, //todo need to move this out but at end of all questions?????
             class,
         });
+        label_iter.nth(3);
     }
     Some(questions)
 }
@@ -101,66 +113,35 @@ mod tests {
         assert_eq!(question.question_type, 1);
         assert_eq!(question.class, 1);
     }
-    
-    // #[test]
-    // fn test_compressed_questions_last_label() {
-    //     let buf = [
-    //         0x03, 0x77, 0x77, 0x77, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x63,
-    //         0x6f, 0x6d, 0x00, 0x00, 0x01, 0x00, 0x01, 
-    //         0x03, 0x61, 0x61, 0x61, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0xc0, 0x18, 0x00, 0x01, 0x00, 0x01,
-    //     ];
-    //     let questions = decode_questions(&buf, 2).unwrap();
-    //     assert_eq!(questions.len(), 2);
-    //     let mut question = &questions[0];
-    //     assert_eq!(question.domain_name.len(), 3);
-    //     assert_eq!(question.domain_name[0].length, 3);
-    //     assert_eq!(question.domain_name[0].content, "www");
-    //     assert_eq!(question.domain_name[1].length, 7);
-    //     assert_eq!(question.domain_name[1].content, "example");
-    //     assert_eq!(question.domain_name[2].length, 3);
-    //     assert_eq!(question.domain_name[2].content, "com");
-    //     assert_eq!(question.question_type, 1);
-    //     assert_eq!(question.class, 1);
-
-    //     question = &questions[1];
-    //     assert_eq!(question.domain_name.len(), 3);
-    //     assert_eq!(question.domain_name[0].length, 3);
-    //     assert_eq!(question.domain_name[0].content, "aaa");
-    //     assert_eq!(question.domain_name[1].length, 7);
-    //     assert_eq!(question.domain_name[1].content, "example");
-    //     assert_eq!(question.domain_name[2].length, 3);
-    //     assert_eq!(question.domain_name[2].content, "com");
-    //     assert_eq!(question.question_type, 1);
-    //     assert_eq!(question.class, 1);
-    // }
 
     #[test]
     fn test_compressed_questions_middle() {
         let buf = [
-            12, 99, 111, 100, 101, 99, 114, 97, 102, 116, 
+            3, 97, 98, 99, 17, 108, 111, 110, 103, 97, 115, 115, 100, 111, 109, 97, 105, 110, 110,
+            97, 109, 101, 3, 99, 111, 109, 0, 0, 1, 0, 1, 3, 100, 101, 102, 192, 16, 0, 1, 0, 1,
         ];
         let questions = decode_questions(&buf, 2).unwrap();
-        assert_eq!(questions.len(), 1);
-        let question = &questions[0];
+        assert_eq!(questions.len(), 2);
+        let mut question = &questions[0];
         assert_eq!(question.domain_name.len(), 3);
         assert_eq!(question.domain_name[0].length, 3);
-        assert_eq!(question.domain_name[0].content, "www");
-        assert_eq!(question.domain_name[1].length, 7);
-        assert_eq!(question.domain_name[1].content, "example");
+        assert_eq!(question.domain_name[0].content, "abc");
+        assert_eq!(question.domain_name[1].length, 17);
+        assert_eq!(question.domain_name[1].content, "longassdomainname");
         assert_eq!(question.domain_name[2].length, 3);
         assert_eq!(question.domain_name[2].content, "com");
         assert_eq!(question.question_type, 1);
         assert_eq!(question.class, 1);
 
-    //     // question = &questions[1];
-    //     // assert_eq!(question.domain_name.len(), 3);
-    //     // assert_eq!(question.domain_name[0].length, 3);
-    //     // assert_eq!(question.domain_name[0].content, "aaa");
-    //     // assert_eq!(question.domain_name[1].length, 7);
-    //     // assert_eq!(question.domain_name[1].content, "example");
-    //     // assert_eq!(question.domain_name[2].length, 3);
-    //     // assert_eq!(question.domain_name[2].content, "com");
-    //     // assert_eq!(question.question_type, 1);
-    //     // assert_eq!(question.class, 1);
+        question = &questions[1];
+        assert_eq!(question.domain_name.len(), 3);
+        assert_eq!(question.domain_name[0].length, 3);
+        assert_eq!(question.domain_name[0].content, "def");
+        assert_eq!(question.domain_name[1].length, 17);
+        assert_eq!(question.domain_name[1].content, "longassdomainname");
+        assert_eq!(question.domain_name[2].length, 3);
+        assert_eq!(question.domain_name[2].content, "com");
+        assert_eq!(question.question_type, 1);
+        assert_eq!(question.class, 1);
     }
 }
