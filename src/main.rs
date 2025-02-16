cbad920149a945b542e13dd2a4bf7e742bbad7a7
burkeow <burkeowen98@gmail.com>
2025-02-16 13:00:38 +1100

    add rust_backtrace to main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use codecrafters_dns_server::dns_server;
-use std::error::Error;
+use std::{env, error::Error};
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -9,6 +9,7 @@ struct Args {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    env::set_var("RUST_BACKTRACE", "1");
     let args = Args::parse();
     println!("Using resolver: {}", args.resolver);
     let mut server = dns_server::server::Server::new("127.0.0.1".to_string(), 2053, args.resolver);
